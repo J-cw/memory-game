32b785417b9b028202de8ca9bcadbaafcf256498
J-cw <jacob.cw@outlook.com>
2022-05-09 02:54:33 +0000

    added stoptime and reset function

--- script.js
+++ script.js
@@ -52,8 +52,9 @@ let seconds = 0;
 let timeStart = false;
 
 function shuffle(array) {
-    let currentIndex = array.length, temporaryValue, randomIndex;
-    while(currentIndex !== 0) {
+    let currentIndex = array.length,
+        temporaryValue, randomIndex;
+    while (currentIndex !== 0) {
         randomIndex = Math.floor(Math.random() * currentIndex);
         currentIndex -= 1;
         temporaryValue = array[currentIndex];
@@ -65,35 +66,35 @@ function shuffle(array) {
 
 function startGame() {
     // Invoke shuffle function and store in variable 
-    
+
     const shuffledDeck = shuffle(deckCards);
-    
+
     // Iterate over deck of cards array
-    
+
     for (let i = 0; i < shuffledDeck.length; i++) {
-       
+
         // Create the <li> tags
-        
+
         const liTag = document.createElement("LI");
-        
+
         // Give <li> class of card
-       
+
         liTag.classList.add("card");
-       
+
         // Create the <img> tags
-       
+
         const addImage = document.createElement("IMG");
-        
+
         // Append <img> to <li>
-       
+
         liTag.appendChild(addImage);
-       
+
         // Set the img src path with the shuffled deck
-       
+
         addImage.setAttribute("src", "img/" + shuffledDeck[i]);
-       
+
         // Add an alt tag to the image
-        
+
         addImage.setAttribute("alt", "image of crown");
 
         // Update the new <li> to the deck <ul>
@@ -101,3 +102,64 @@ function startGame() {
         deck.appendChild(liTag);
     }
 }
+
+startGame();
+
+function removeCard() {
+    while (deck.hasChildNodes()) {
+        deck.removeChild(deck.firstChild);
+    }
+}
+
+function timer() {
+    time = setInterval(function () {
+        seconds++;
+        if (seconds === 60) {
+            minutes++;
+            seconds = 0;
+        }
+        timeCounter.innerHTML = "<i class='fa fa-hourglass-start'></i>" + " Timer: " + minutes +
+            " Mins " + seconds + " Secs";
+    }, 1000);
+}
+
+function stopTime() {
+    clearInterval(time);
+}
+
+function resetEverything() {
+    stopTime();
+    timeStart = false;
+    seconds = 0;
+    minutes = 0;
+    timeCounter.innerHTML = "<i class='fa fa-hourglass-start'></i>" + " Timer: 00:00";
+
+    // Reset star count and then add the class back to show stars again
+
+    star[1].firstElementChild.classList.add("fa-star");
+
+    star[2].firstElementChild.classList.add("fa-star");
+
+    starCount = 3;
+
+    // Reset moves count and reset its inner HTML
+
+    moves = 0;
+
+    movesCount.innerHTML = 0;
+
+    // Clear both arrays that hold the opened and matched cards
+
+    matched = [];
+
+    opened = [];
+
+    // Clear the deck
+
+    removeCard();
+
+    // Create a new deck
+
+    startGame();
+}
+
