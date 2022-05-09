a7d342b655f272824f40ea9eb52252ad49f3eb63
J-cw <jacob.cw@outlook.com>
2022-05-09 03:41:11 +0000

    changing card sizes to fit nicer

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,9 @@
 // Array of Deck of Card Images
 
-const deckCards = ["imgs/jackClubs.png", "imgs/jackClubs.png", "imgs/jackDiamonds.png", "imgs/jackDiamonds.png",
-    "imgs/jackHearts.png", "imgs/jackHearts.png", "imgs/kingDiamonds.png", "imgs/kingDiamonds.png", "imgs/kingHearts.png",
-    "imgs/kingHearts.png", "imgs/kingSpades.png", "imgs/kingSpades.png", "imgs/queenClubs.png", "imgs/queenClubs.png",
-    "imgs/queenSpades.png", "imgs/queenSpades.png"
+const deckCards = ["../imgs/jackClubs.png", "../imgs/jackClubs.png", "../imgs/jackDiamonds.png", "../imgs/jackDiamonds.png",
+    "../imgs/jackHearts.png", "../imgs/jackHearts.png", "../imgs/kingDiamonds.png", "../imgs/kingDiamonds.png", "../imgs/kingHearts.png",
+    "../imgs/kingHearts.png", "../imgs/kingSpades.png", "../imgs/kingSpades.png", "../imgs/queenClubs.png", "../imgs/queenClubs.png",
+    "../imgs/queenSpades.png", "../imgs/queenSpades.png"
 ];
 
 // Global Arrays
