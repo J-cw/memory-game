593806ca70b62e12f20479dd2a1af900ccc7cea8
J-cw <jacob.cw@outlook.com>
2022-05-09 02:37:54 +0000

    adding relavant js query selectors

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
 // Array of Deck of Card Images
 
-const deckCards = ["imgs/playing-card-g1c93dfc44_1280.png"]
+const deckCards = ["imgs/jackClubs.png", "imgs/jackClubs.png", "imgs/jackDiamonds.png", "imgs/jackDiamonds.png",
+    "imgs/jackHearts.png", "imgs/jackHearts.png", "imgs/kingDiamonds.png", "imgs/kingDiamonds.png", "imgs/kingHearts.png",
+    "imgs/kingHearts.png", "imgs/kingSpades.png", "imgs/kingSpades.png", "imgs/queenClubs.png", "imgs/queenClubs.png",
+    "imgs/queenSpades.png", "imgs/queenSpades.png"
+];
+
+// Global Arrays
+
+// Access the <ul> with class of .deck
+
+const deck = document.querySelector(".deck");
+
+// Create an empty array to store the opened cards
+
+let opened = [];
+
+// Create an empty array to store the matched cards
