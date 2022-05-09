3f68b5d00c3a80b5e5e36427cba900e39ee6fec1
J-cw <jacob.cw@outlook.com>
2022-05-09 02:41:03 +0000

    fixed spelling mistake and added timer variables along with shuffle function

--- script.js
+++ script.js
@@ -17,3 +17,49 @@ const deck = document.querySelector(".deck");
 let opened = [];
 
 // Create an empty array to store the matched cards
+
+let matched = [];
+
+// Access the modal
+
+const modal = document.getElementById("modal");
+
+// Access the reset button
+
+const reset = document.querySelector(".reset-btn");
+
+// Access the play again button 
+
+const playAgain = document.querySelector(".play-again-btn");
+
+// Select the class moves-counter and change its HTML
+
+const movesCount = document.querySelector(".moves-counter");
+
+// Create variable for moves counter, start the count at zero
+
+let moves = 0;
+
+const star = document.getElementById("rating").querySelectorAll(".star");
+
+let starCount = 3;
+
+const timeCounter = document.querySelector(".timer");
+
+let time;
+let minutes = 0;
+let seconds = 0;
+let timeStart = false;
+
+function shuffle(array) {
+    let currentIndex = array.length, temporaryValue, randomIndex;
+    while(currentIndex !== 0) {
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+    return array;
+}
+
